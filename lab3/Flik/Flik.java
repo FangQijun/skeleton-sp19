5a0b60f370106458ec138e1dce15482accc1d4c1
Francis Fang <frank_fang2011@live.cn>
2022-03-15 00:31:26 -0400

    Finished lab3 Part Flik.

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,6 @@
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        return a.equals(b);  // https://stackoverflow.com/questions/1700081/why-is-128-128-false-but-127-127-is-true-when-comparing-integer-wrappers-in-ja
     }
 }
